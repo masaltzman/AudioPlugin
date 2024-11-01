199355c3e3446b1ed89df2c6f540c33c3d17f3e9
mark 6173655099 <mark@marks-MacBook-Pro-16.local>
2024-11-01 10:38:47 -0400

    properly generate compile_commands.json using temporary ninja build, for intellisense

--- cmake-macos-config.sh
+++ cmake-macos-config.sh
@@ -1,4 +1,4 @@
 rm -rf build
 mkdir build
-cmake -S . -B build -G Xcode -DCMAKE_OSX_SYSROOT=macosx -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
-cp etc/compile_commands.json build
+cmake -S . -B build -G Xcode -DCMAKE_OSX_SYSROOT=macosx
+
--- create-compile-commands.sh
+++ create-compile-commands.sh
@@ -1,4 +1,5 @@
 cmake -S . -B build-ninja -G Ninja -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
 sed -i '' 's|/build-ninja|/build|g' build-ninja/compile_commands.json
-cp build-ninja/compile_commands.json build/
+cp build-ninja/compile_commands.json . 
+cp compile_commands.json build/
 rm -rf build-ninja
